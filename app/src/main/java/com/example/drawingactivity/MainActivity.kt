b0cae34367ac613438c4d852acb875843322db01
BrossCode <89676904+BrossCode@users.noreply.github.com>
2022-09-22 18:54:07 -0700

    FA-17 Drawable Area and Drawing Widget

--- app/src/main/java/com/example/drawingactivity/MainActivity.kt
+++ app/src/main/java/com/example/drawingactivity/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.drawingactivity
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel = CustomColorViewModel()
+    private val viewModel: CustomColorViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val drawableArea = findViewById<DrawableAreaView>(R.id.drawableArea)
+        drawableArea.onBitmapDrawn = { bitmap -> viewModel.saveUserBitmap(bitmap) }
+        viewModel.currentBitmap?.let { drawableArea.setBitmap(it) }
         val colorSelector = findViewById<PrimaryColorCircleView>(R.id.colorCircle)
         colorSelector.onChangeColor = { viewModel.cyclePrimaryColor() }
 
         viewModel.onUpdate = {
             colorSelector.circlePrimaryColor = it.primaryColor
+            drawableArea.pathPaint.color = it.primaryColor
+            drawableArea.userDrawnBitmap = it.currentBitmap
         }
     }
 }
